7deb7f045e0e54a8a910c3a7bcd6b27602a157f4
Christina Leisen <cil25@drexel.edu>
2022-11-01 14:26:44 -0400

    header and top of page

---
+++ final/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://use.typekit.net/xqu1ubt.css">
+    <title>Final</title>
+</head>
+<body>
+
+<header class="header">
+    <div class="header__cont">
+        <div class="logo">
+            <h1>lemon & basil</h1>
+        </div>
+        <div class="login">
+            LOGIN
+        </div>
+    </div>
+</header>
+    
+    
--- final/index.php
+++ final/index.php
@@ -1,8 +1,58 @@
-<?php include_once 'components/header.php';?>
+<?php include_once 'header.php';?>
 
-<?php $test = 'Assignment - 01'; ?>
+<!-- <?php $test = 'Assignment - 01'; ?>
 <h1>
     <?php echo $test; ?>
-</h1>
+</h1> -->
 
-<?php include_once 'components/footer.php';?>
+<main>
+    <section class="top">
+        <div class="info__card">
+            <div class="recipe__title">
+            Broccoli and Chicken Stir-Fry
+            </div>
+            <div class="info">
+                <ul>
+                    <li>Difficulty: 
+                        <span class="dot__filled"></span>
+                        <span class="dot__filled"></span>
+                        <span class="dot__empty"></span>
+                        <span class="dot__empty"></span>
+                        <span class="dot__empty"></span>
+                    </li>
+                    <li>Time to Prep: <span>15 mins</span></li>
+                    <li>Time to Cook: <span>20 mins</span></li>
+                    <li>Total Time: <span>35 mins</span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="recipe__image">
+            <img src="images/broccoli-and-chicken.png">
+        </div>
+    </section>
+
+    <section class="ingredient__section">
+        <div class="center__card">
+            <div class="center__subtitle">
+                Ingredients
+            </div>
+            <div class="ingredients">
+                <ul>
+                    <li>2/3 cup <span>soy sauce</span></li>
+                    <li>1/4 cup <span>brown sugar</span></li>
+                    <li>1/2 teaspoon <span>ground ginger</span></li>
+                    <li>1 pinch <span>red pepper flakes,</span> or to taste</li>
+                    <li>2 tablespoons <span>water</span></li>
+                    <li>2 teaspoons <span>vegetable oil,</span> or to taste</li>
+                    <li>3 skinless, <span>boneless chicken breast</span> halves, cut into chunks</li>
+                    <li>1 <span>onion</span>, sliced</li>
+                    <li>3 cups <span>broccoli florets</span></li>
+                    <li>2 tablespoons <span>cornstarch</span></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php include_once 'footer.php';?>
